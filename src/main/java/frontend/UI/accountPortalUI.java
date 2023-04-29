edf04edaf22017e4306ab80e21c381a105756476
Luke Smith <luke_smith10@baylor.edu>
2023-04-28 22:08:21 -0500

    Feat: TABLE NOW WORKS AND CAN MODIFY RESERVATION AND MAKE A NEW ONE LFGGGGGGGGGGGGGGGGG

--- src/main/java/frontend/UI/accountPortalUI.java
+++ src/main/java/frontend/UI/accountPortalUI.java
@@ -32,6 +32,7 @@ public class accountPortalUI extends JFrame implements ActionListener {
     private JButton cancelReservationButton;
 
     // Table Attributes
+    private JLabel reservationStatusLabel;
     private JTable table;
 
 
@@ -40,30 +41,33 @@ public class accountPortalUI extends JFrame implements ActionListener {
         UIBlackBox.saveAll();
 
         panel = new JPanel();
-        panel.setBorder(BorderFactory.createTitledBorder( BorderFactory.createEtchedBorder()));
+        //panel.setBorder(BorderFactory.createTitledBorder( BorderFactory.createEtchedBorder()));
 
-        String[] columnHeader = {"Room Number", "Room Size"};
+        String[] columnHeader = {"Room Number", "Room Size", "Start Date", "End Date"};
         String[][] data = {
-                {"123", "Suite"},
-                {"69", "Single King"},
-                {"20", "Double King"}
+                {"123", "Suite", "5/1/2023", "5/6/2023"},
+                {"69", "Single King", "5/1/2023", "5/6/2023"},
+                {"20", "Double King", "5/1/2023", "5/6/2023"}
         };
+
         TableModel model = new DefaultTableModel(data, columnHeader) {
             public boolean isCellEditable(int rowIndex, int mColIndex) {
                 return false;
             }
         };
         table = new JTable(model);
-        table.setPreferredScrollableViewportSize(new Dimension(500, 50));
+        table.setPreferredScrollableViewportSize(new Dimension(500, 150));
         table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-        table.setFillsViewportHeight(false);
+        table.setFillsViewportHeight(true);
 
         panel.add(new JScrollPane(table));
-
 
         // Set All Components
         greetingLabel = new JLabel("Hello, " + UIBlackBox.getCurrentAccount().getFirstName()); // TODO: Add Hello, "user's name"
         greetingLabel.setFont(new Font("Barlow", Font.BOLD, 30));
+
+        reservationStatusLabel = new JLabel("Current Reservations:");
+        reservationStatusLabel.setFont(new Font("Barlow", Font.PLAIN, 20));
 
         // Add and overwrite sign out button
         signOutButton = new JButton("Sign Out");
@@ -84,7 +88,6 @@ public class accountPortalUI extends JFrame implements ActionListener {
         });
 
 
-
         // Button for creating new reservation
         newReservationButton = new JButton("New Reservation");
         newReservationButton.addActionListener(new ActionListener() {
@@ -96,8 +99,26 @@ public class accountPortalUI extends JFrame implements ActionListener {
             }
         });
 
+
         cancelReservationButton = new JButton("Cancel Reservation");
-        //cancelReservationButton.addActionListener();
+        cancelReservationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int viewRow = table.getSelectedRow();
+                if (viewRow < 0) {
+                    JOptionPane.showMessageDialog(null, "No Row Selected!");
+                } else {
+                    int modelRow = table.convertRowIndexToModel(viewRow);
+                    DefaultTableModel model = (DefaultTableModel) table.getModel();
+                    int answer = JOptionPane.showConfirmDialog(null, "Are you sure you want to cancel your reservation for " +
+                                    model.getValueAt(modelRow, 2) + " to " + model.getValueAt(modelRow, 3) + "?",
+                            "Warning", JOptionPane.YES_NO_OPTION);
+                    if (answer == 0) {
+                        model.removeRow(modelRow);
+                    }
+                }
+            }
+        });
 
         // Add and set container
         container = getContentPane();
@@ -108,16 +129,20 @@ public class accountPortalUI extends JFrame implements ActionListener {
 
     // Sets all labels/fields bounds
     public void setBounds() {
-        greetingLabel.setBounds(50, 10, 250, 30);
-        signOutButton.setBounds(300, 10, 100, 30);
-        newReservationButton.setBounds(50, 60, 200, 30);
-        panel.setBounds(50, 90, 500, 100);
+        greetingLabel.setBounds(25, 10, 250, 30);
+        reservationStatusLabel.setBounds(25, 80, 200, 30);
+        signOutButton.setBounds(425, 10, 100, 30);
+        newReservationButton.setBounds(25, 250, 150, 30);
+        cancelReservationButton.setBounds(175, 250, 150, 30);
+        panel.setBounds(25, 100, 500, 150);
     }
 
     public void addComponents() {
         container.add(greetingLabel);
+        container.add(reservationStatusLabel);
         container.add(signOutButton);
-        //container.add(newReservationButton);
+        container.add(newReservationButton);
+        container.add(cancelReservationButton);
         container.add(panel);
     }
 
@@ -125,7 +150,7 @@ public class accountPortalUI extends JFrame implements ActionListener {
         accountPortalUI frame = new accountPortalUI();
         frame.setTitle("Account Portal");
         frame.setVisible(true);
-        frame.setBounds(500, 100, 600, 400);
+        frame.setBounds(500, 100, 575, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(true);
     }
